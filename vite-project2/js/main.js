b04d09bb8f5b085cd18ac693c582115293a8960a
DariaG16 <113132247+DariaG16@users.noreply.github.com>
2023-01-13 18:02:23 -0500

    yes

--- vite-project2/js/main.js
+++ vite-project2/js/main.js
@@ -1,13 +1,36 @@
 import "../styles/style.css";
-const URL = "https://valorant-api.com/v1/agents";
+const URL = `https://poetrydb.org/title/6/lines.json`;
 
 async function getData(URL) {
   try {
     const response = await fetch(URL);
-    const data = await response.json(); //makes the data into a json object u can use
-    console.log(response);
+    const everything = await response.json(); //makes the data into a json object u can use
+    console.log(everything);
   } catch (error) {
     console.log(error);
   }
 }
 getData(URL);
+
+function removeCard() {
+  const cards = document.querySelectorAll(".Category");
+  const cardsArray = Array.from(cards);
+  cardsArray.forEach((cards) => {
+    cards.remove();
+  });
+}
+function makeCards(type) {
+  removeCard();
+  type.forEach((cookies) =>
+    DOMselectors.everything.insertAdjacentHTML(
+      "beforeend",
+      `<div class="Category cards" >
+    <h3>Type: ${idk1}</h3>
+    <h3>Rarity: ${idk2}</h3>
+    <h3>Useful In: ${idk3}</h3>
+    <img class="images" src="${idk4}" alt="${idk5}">
+    <h2>${idk6}</h2>
+    </div>`
+    )
+  );
+}
