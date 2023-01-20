e9c4ee77ce7264ac35d1aec7232c41a6193e98f3
DariaG16 <dariagolitsyna16@gmail.com>
2023-01-20 14:00:09 -0500

    I DID SMTG YESSSS

--- vite-project2/js/main.js
+++ vite-project2/js/main.js
@@ -8,24 +8,30 @@ const DOMSelectors = {
   author: document.querySelector("#Author"),
   contentsbtn: document.querySelector("#Contents"),
   titlebtn: document.querySelector("#Title"),
-  AOLbtn: document.querySelector("#AmountOfLines"),
   randombtn: document.querySelector("#Random"),
   search: document.querySelector("#search"),
   searchtxt: document.querySelector("#searchbartxt"),
 };
 DOMSelectors.author.addEventListener("click", function () {
-  //let filterBy = "author";
-  //return filterBy;
   getData();
 });
-
+DOMSelectors.contentsbtn.addEventListener("click", function () {
+  getData();
+});
+DOMSelectors.titlebtn.addEventListener("click", function () {
+  getData();
+});
+DOMSelectors.randombtn.addEventListener("click", function () {
+  getData();
+});
 DOMSelectors.search.addEventListener("click", function () {
   let input = DOMSelectors.form.value;
   console.log(input);
 });
+
 async function getData() {
   try {
-    let filterBy = "author";
+    let filterBy = "title";
     const response = await fetch(`https://poetrydb.org/${filterBy}`);
     const everything = await response.json(); //makes the data into a json object u can use
     console.log(everything);
@@ -49,36 +55,12 @@ DOMSelectors.author.addEventListener("click", function () {
     cards.remove();
   });
 }*/
-function makeCards1(type) {
-  type.forEach(() =>
-    DOMSelectors.cards.insertAdjacentHTML(
-      "afterend",
-      `<div class="Category cards" >
-    <h3>Type: YES</h3>
-    </div>`
-    )
-  );
-}
+
 function makeCards(something) {
   DOMSelectors.cards.insertAdjacentHTML(
     "afterend",
     `<div class="Category cards" >
-      <h3>list of authors${something.authors}</h3>
+      <h3>list of authors${something.titles}</h3>
       </div>`
-  );
-}
-function makeCardds(type) {
-  removeCard();
-  type.forEach((cookies) =>
-    DOMselectors.everything.insertAdjacentHTML(
-      "beforeend",
-      `<div class="Category cards" >
-    <h3>Type: ${cookies.type}</h3>
-    <h3>Rarity: ${cookies.rarity}</h3>
-    <h3>Useful In: ${cookies.usefulIn}</h3>
-    <img class="images" src="${cookies.img}" alt="${cookies.name}">
-    <h2>${cookies.name}</h2>
-    </div>`
-    )
   );
 }
