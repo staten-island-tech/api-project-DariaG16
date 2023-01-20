6ef3ba5377005e1e7cbfea4611d002b4a6c0940d
DariaG16 <dariagolitsyna16@gmail.com>
2023-01-20 13:48:38 -0500

    i did SOMETHING

--- vite-project2/js/main.js
+++ vite-project2/js/main.js
@@ -6,71 +6,81 @@ const DOMSelectors = {
   form: document.querySelector("#form"),
   cards: document.querySelector(".cards"),
   author: document.querySelector("#Author"),
-  contents: document.querySelector("#Contents"),
-  title: document.querySelector("#Title"),
-  amountOfLines: document.querySelector("#AmountOfLines"),
-  random: document.querySelector("#Random"),
   contentsbtn: document.querySelector("#Contents"),
   titlebtn: document.querySelector("#Title"),
   AOLbtn: document.querySelector("#AmountOfLines"),
   randombtn: document.querySelector("#Random"),
   search: document.querySelector("#search"),
   searchtxt: document.querySelector("#searchbartxt"),
-  search: document.querySelector("#search"),
 };
 DOMSelectors.author.addEventListener("click", function () {
-  let filterBy = "author";
-  return filterBy;
+  //let filterBy = "author";
+  //return filterBy;
   getData();
 });
+
 DOMSelectors.search.addEventListener("click", function () {
   let input = DOMSelectors.form.value;
   console.log(input);
 });
 async function getData() {
   try {
-    const response = await fetch(
-      `https://poetrydb.org/${filterBy}/edgar allan poe`
-    );
+    let filterBy = "author";
+    const response = await fetch(`https://poetrydb.org/${filterBy}`);
     const everything = await response.json(); //makes the data into a json object u can use
     console.log(everything);
+    makeCards(everything);
   } catch (error) {
     console.log(error);
   }
 }
 //getData(`https://poetrydb.org/${filterBy}/${input}`);
 
-/* DOMSelectors.author.addEventListener("click", author);
-function author() {
-  console.log(`${URL}`);
-  // DOMSelectors.form.insertAdjacentHTML("afterend");
-}
- */
 DOMSelectors.author.addEventListener("click", function () {
   //when the button clicks, do this function:
   // we get the value of whatever the user typed in the text box named "expression"
   //makeCards(input);
 });
-/* function crying() {
-  document.getElementById("Author").innerHTML += "First function was executed!";
-}
-DOMSelectors.contents.addEventListener("click", crying());
- */
+
 /* function removeCard() {
   const cards = document.querySelectorAll(".Category");
   const cardsArray = Array.from(cards);
   cardsArray.forEach((cards) => {
     cards.remove();
   });
-}
-function makeCards(type) {
-  removeCard();
-  type.forEach((author) =>
-    DOMselectors.form.insertAdjacentHTML(
+}*/
+function makeCards1(type) {
+  type.forEach(() =>
+    DOMSelectors.cards.insertAdjacentHTML(
       "afterend",
       `<div class="Category cards" >
-    <h3>Type: ${poems.author}</h3>
+    <h3>Type: YES</h3>
     </div>`
     )
   );
-} */
+}
+function makeCards(something) {
+  something.forEach((name) =>
+    DOMSelectors.cards.insertAdjacentHTML(
+      "afterend",
+      `<div class="Category cards" >
+      <h3>Type: YES ${name.authors}</h3>
+      </div>`
+    )
+  );
+}
+function makeCardds(type) {
+  removeCard();
+  type.forEach((cookies) =>
+    DOMselectors.everything.insertAdjacentHTML(
+      "beforeend",
+      `<div class="Category cards" >
+    <h3>Type: ${cookies.type}</h3>
+    <h3>Rarity: ${cookies.rarity}</h3>
+    <h3>Useful In: ${cookies.usefulIn}</h3>
+    <img class="images" src="${cookies.img}" alt="${cookies.name}">
+    <h2>${cookies.name}</h2>
+    </div>`
+    )
+  );
+}
